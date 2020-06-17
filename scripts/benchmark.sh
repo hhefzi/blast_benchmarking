e61390c8d88119cd97c3b9b050f2eb74a15c9c99
Hooman Hefzi <hooman.hefzi@gmail.com>
2020-06-17 11:44:35 -0700

    init results

--- scripts/benchmark.sh
+++ scripts/benchmark.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 
-for p in $(ls ./data/*)
+for t in 15 8 4 2 1
 do
-for t in 1 2 4 8 15
+for p in $(ls ./data/*)
 do
 echo "$p $t"
 echo "$p $t" >> ./output/times.txt
-/usr/bin/time -f "%e" -o ./times.txt -a blastp -query $p -db /data/cho_protein/GCF_003668045.1_CriGri-PICR_protein.faa -outfmt 6 -out tmp.blast -num_threads $t
+/usr/bin/time -f "%e" -o ./output/times.txt -a blastp -query $p -db /data/cho_protein/GCF_003668045.1_CriGri-PICR_protein.faa -outfmt 6 -out tmp.blast -num_threads $t
 done
 done
